b0065a81519c796dbc883be3ac3f5313b6a99e78
lingtao-sa <83944109+lingtao-sa@users.noreply.github.com>
2022-01-28 17:36:27 +1100

    Update init-ml-azure-vm.sh

--- shell/init-ml-azure-vm.sh
+++ shell/init-ml-azure-vm.sh
@@ -1,20 +1,21 @@
-# Copyright 2022-2018 MarkLogic Corporation.  All Rights Reserved.
-#
-#!/bin/bash
-######################################################################################################
-# File         : init-ml-azure-vm.sh
-# Description  : This script will setup the first node in the cluster
-# Usage        : sh init-bootstrap-node.sh username password auth-mode \
-#                n-retry retry-interval security-realm hostname
-######################################################################################################
-
-
-# Wait for ML to start
-#sleep 2m
+sleep 2m
 
 # Update CentOS - It will take quit a while to complete 
-sudo yum -y update
+#sudo yum -y update
 
 # Init ML bootstrap node
 sh init-bootstrap-node.sh 'admin' 'admin' 'basic' \
     3 10 'public' localhost
+
+# Wait for ML to Init
+sleep 2m
+
+# Creat datadog user in ML DB
+curl -X POST --anyauth --user admin:admin -i -H "Content-Type: application/json" -d '{"user-name": "datadog", "password": "T3m@sek#", "roles": {"role": "manage-user"}}' http://localhost:8002/manage/v2/users    
+
+# Deploy pre-configed datadog yaml files
+cp -f /var/lib/waagent/custom-script/download/0/datadog.yaml /etc/datadog-agent/
+cp -f /var/lib/waagent/custom-script/download/0/conf.yaml /etc/datadog-agent/config.g/marklogic.d/
+
+# Restart datadog agent
+sudo systemctl restart datadog-agent
